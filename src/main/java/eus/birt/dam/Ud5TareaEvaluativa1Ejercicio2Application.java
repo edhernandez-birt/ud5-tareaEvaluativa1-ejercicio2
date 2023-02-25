0ade17f4cacc6c777b1ab66537c010b3f6ad8aba
Eduardo Hernandez <edhernandez@birt.eus>
2023-02-25 10:45:51 +0100

    Commit inicial. Creado proyecto Springboot

---
+++ src/main/java/eus/birt/dam/Ud5TareaEvaluativa1Ejercicio2Application.java
@@ -0,0 +1,13 @@
+package eus.birt.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud5TareaEvaluativa1Ejercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud5TareaEvaluativa1Ejercicio2Application.class, args);
+	}
+
+}
